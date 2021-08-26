6feea6aac08676fe60010e3c0d411f6b9c31c485
terence-octo <86947803+terence-octo@users.noreply.github.com>
2021-08-26 11:18:54 +0930

    Update deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f Dockerfile -t terenceocto/random-quotes-azure:latest .
-sudo docker push terenceocto:latest
+sudo docker push terenceocto/random-quotes-azure:latest
